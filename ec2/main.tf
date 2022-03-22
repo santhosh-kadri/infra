9942eaa6f86f30bf957f0582a648f0cd4c087031
santhosh <santhosh.kadri@hotmail.com>
2022-03-22 22:47:41 +0530

    create a ec2 instance

---
+++ ec2/main.tf
@@ -0,0 +1,23 @@
+terraform{
+    required_providers{
+        aws = {
+            source = "hashicorp/aws"
+            version ="~> 3.27"
+        }
+    }
+
+    required_version = ">=0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "app_server"{
+    ami = "ami-0851b76e8b1bce90b"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "TestingTerraFormInstacness"
+    }
+}
