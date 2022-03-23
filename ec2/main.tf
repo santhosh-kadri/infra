1471b06c17977dee71ad6201ef9537521d74f71a
santhosh <santhosh.kadri@hotmail.com>
2022-03-24 00:34:55 +0530

    dev

--- ec2/main.tf
+++ ec2/main.tf
@@ -17,9 +17,8 @@ provider "aws" {
 resource "aws_instance" "app_server"{
     ami = "ami-0851b76e8b1bce90b"
     instance_type = "t2.micro"
-    vpc_id = "vpc-037800a0f6733d801"
-    cidr_block = "10.0.1.0/24"
-
+    vpc = "vpc-037800a0f6733d801"
+   
 
     tags = {
         Name = "TestingTerraFormInstacness"
