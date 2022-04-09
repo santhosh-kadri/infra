e5abb5c8393fec52d27690e09b3af6e2c195a2f8
santhosh <santhosh.kadri@hotmail.com>
2022-04-09 16:12:28 +0530

    added ecr peop policy

--- ecr/create_ecr_repo.tf
+++ ecr/create_ecr_repo.tf
@@ -30,8 +30,22 @@ resource "aws_ecr_repository_policy" "sf-repo-policy" {
                 "Sid": "ECR Repository Policy",
                 "Effect": "Allow",
                 "Principal": "*",
-                "Action": ["ecr:DescribeImages",
-                        "ecr:DescribeRepositories"]
+                "Action": [
+                    "ecr:GetDownloadUrlForLayer",
+                    "ecr:BatchGetImage",
+                    "ecr:BatchCheckLayerAvailability",
+                    "ecr:PutImage",
+                    "ecr:InitiateLayerUpload",
+                    "ecr:UploadLayerPart",
+                    "ecr:CompleteLayerUpload",
+                    "ecr:DescribeRepositories",
+                    "ecr:GetRepositoryPolicy",
+                    "ecr:ListImages",
+                    "ecr:DeleteRepository",
+                    "ecr:BatchDeleteImage",
+                    "ecr:SetRepositoryPolicy",
+                    "ecr:DeleteRepositoryPolicy"
+                ]
             }]
         }
     EOF
